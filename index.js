9702d968efa1ad316b5b2359acd73ae819827354
smcleman <stuart@stuartmcleman.com>
2015-10-28 13:02:30 +0000

    Add write stream "finish" event handler
    In testing particularly long image resize operations it was observed that sometimes the resulting file was size zero 
    On the "finish" event call the write callback. This should make sure the stream has been flushed to the system as per the Node.js docs.

--- index.js
+++ index.js
@@ -1458,6 +1458,8 @@ Jimp.prototype.write = function (path, cb) {
         stream.on("open", function(fh) {
             stream.write(buffer);
             stream.end();
+        });
+        stream.on("finish", function(fh) {
             return cb.call(that, null, that);
         });
     });
